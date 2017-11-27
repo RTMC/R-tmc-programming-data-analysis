da4930dcd4256324ffd6089f5796c9f161b8cdc0
Aleksis Vuoksenmaa <aleksis.vuoksenmaa@helsinki.fi>
2017-11-27 11:39:50 +0200

    Added solutions

--- part1/R/arithmetics.R
+++ part1/R/arithmetics.R
@@ -1,16 +1,28 @@
 
 add <- function(a, b) {
+  # BEGIN SOLUTION
   return(a+b)
+  # END SOLUTION
+  # STUB: return(0)
 }
 
 subtract <- function(a, b) {
+  # BEGIN SOLUTION
   return(a-b)
+  # END SOLUTION
+  # STUB: return(0)
 }
 
 multiply <- function(a, b) {
+  # BEGIN SOLUTION
   return(a*b)
+  # END SOLUTION
+  # STUB: return(0)
 }
 
 divide <- function(a, b) {
+  # BEGIN SOLUTION
   return(a/b)
+  # END SOLUTION
+  # STUB: return(0)
 }
--- part1/R/strings.R
+++ part1/R/strings.R
@@ -1,3 +1,6 @@
 constant_string <- function() {
+    # BEGIN SOLUTION
     return("jono")
+    # END SOLUTION
+    # STUB: return("a")
 }
